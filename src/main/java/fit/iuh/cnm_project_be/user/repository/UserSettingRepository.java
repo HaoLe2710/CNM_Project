6161f84560ddc50f91bf4fd3ebee177f23fce3a3
Minhduy407 <minhdii1510@gmail.com>
2026-05-01 21:18:02 +0700

    refactor: modularize auth services and implement user settings functionality

--- src/main/java/fit/iuh/cnm_project_be/user/repository/UserSettingRepository.java
+++ src/main/java/fit/iuh/cnm_project_be/user/repository/UserSettingRepository.java
@@ -5,10 +5,13 @@ import fit.iuh.cnm_project_be.user.entity.UserSetting;
 import fit.iuh.cnm_project_be.user.entity.UserSettingId;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserSettingRepository
         extends BaseRepository<UserSetting, UserSettingId> {
 
     List<UserSetting> findByUserId(UUID userId);
+
+    Optional<UserSetting> findByUserIdAndKey(UUID userId, String key);
 }
